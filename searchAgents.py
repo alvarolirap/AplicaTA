c925ebca7fa9c04c1fa6187f13ce5073b2ab59be
Alvaro Lira Palomino <38359627+alvarolirap@users.noreply.github.com>
2018-05-09 16:27:06 -0500

    Update searchAgents.py

--- searchAgents.py
+++ searchAgents.py
@@ -98,7 +98,6 @@ class SearchAgent(Agent):
 			raise AttributeError, prob + ' is not a search problem type in SearchAgents.py.'
 		self.searchType = globals()[prob]
 		print('[SearchAgent] using problem type ' + prob)
-        print 'Hola'
 
     def registerInitialState(self, state):
         """
